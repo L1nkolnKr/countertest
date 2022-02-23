e4e1d2ecb99909831b892650e58434fc9b895fcc
l1nkolnkr <mr.aleks111@gmail.com>
2022-02-23 13:40:13 +0200

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [value, setValue] = useState(0)
+
+    useEffect(() => {
+        getFromLocalStorageHandler()
+    }, [])
+
+    useEffect(()=>{
+        setToLocalStorageHandler()
+    },[value])
+
+    const incHandler = () => {
+        setValue(value + 1)
+    }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('counterValue', JSON.stringify(value))
+    }
+
+    const getFromLocalStorageHandler = () => {
+        let valueAsString = localStorage.getItem('counterValue')
+        if(valueAsString){
+            let newValue = JSON.parse(valueAsString)
+            setValue(newValue)
+        }
+    }
+
+
+    return (
+        <div className="App">
+            <h1>{value}</h1>
+            <button onClick={incHandler}>Increment</button>
+        </div>
+    );
 }
 
 export default App;
